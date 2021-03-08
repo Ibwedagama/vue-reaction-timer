18e604d9f781181e45d1357736c96fef0c67a556
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-03-08 09:29:02 +0800

    make changes

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,40 +1,41 @@
 <template>
-	<p>Reaction Time: {{ score }} ms</p>
-	<h3>
-		Your Rank : <span>{{ rank }}</span>
-	</h3>
+  <p>Reaction Time: {{ score }} ms</p>
+  <h3>
+    Your Rank : <span>{{ rank }}</span>
+  </h3>
 </template>
 
 <script>
 export default {
-	props: ['score'],
-	data() {
-		return {
-			rank: null,
-		}
-	},
-	mounted() {
-		if (this.score < 250) {
-			this.rank = 'Ninja Fingers'
-		} else if (this.score < 400) {
-			this.rank = 'Rapid Reflexes'
-		} else {
-			this.rank = 'Snail pace...'
-		}
-	},
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+      test: '',
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  },
 }
 </script>
 
 <style scoped>
 p {
-	font-size: 1.4rem;
-	color: #444;
+  font-size: 1.4rem;
+  color: #444;
 }
 h3 {
-	font-size: 2rem;
-	color: #333;
+  font-size: 2rem;
+  color: #333;
 }
 h3 span {
-	color: mediumseagreen;
+  color: mediumseagreen;
 }
 </style>
